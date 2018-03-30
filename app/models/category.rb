514ba76e0fa04f7e578710e653d6e764e6b0d279
Rizwan Kadiwal <rizwan.kadiwal@yahoo.ca>
2018-03-30 14:30:18 -0500

    Implementing has many through relation btwn products and categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,9 @@
 class Category < ApplicationRecord
+  has_many :categories_products
+  has_many :products, through: :categories_products
+  validates :category_name, presence: true
+  has_attached_file :category_image, styles: {cover:"850x360>"}, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :category_image, content_type: /\Aimage\/.*\z/
+  extend ActsAsTree::TreeView
+  acts_as_tree order: "category_name"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180330192230) do
+ActiveRecord::Schema.define(version: 20180330192702) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category_name"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20180330192230) do
     t.integer "category_image_file_size"
     t.datetime "category_image_updated_at"
     t.integer "parent_id"
+  end
+
+  create_table "categories_products", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_categories_products_on_category_id"
+    t.index ["product_id"], name: "index_categories_products_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
