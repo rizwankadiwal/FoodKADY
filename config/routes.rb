b731a3a713bd38bebce3b623acb81b1e50ed6ea5
Rizwan Kadiwal <rizwan.kadiwal@yahoo.ca>
2018-04-02 23:49:33 -0500

    Initiating Front End Store

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
     root controller: :dashboard, action: :index
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root controller: :application, action: :index
+
 end
