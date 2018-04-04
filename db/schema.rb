c046473f100ce8e7054139a890871e309d2273fe
Rizwan Kadiwal <rizwan.kadiwal@yahoo.ca>
2018-04-04 00:39:24 -0500

    Added name and phone_no columns to customer table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403192747) do
+ActiveRecord::Schema.define(version: 20180404053605) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category_name"
@@ -46,6 +46,10 @@ ActiveRecord::Schema.define(version: 20180403192747) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_no"
+    t.string "int"
+    t.string "full_name"
+    t.string "string"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
